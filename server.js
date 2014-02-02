ad671ba2fc1eef2c0aef16e3f0e83273b48f3235
Thorsteinn Gestsson <oddimatrix@gmail.com>
2014-02-02 03:07:45 +0100

    Refactoring server.js 2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-var config = require('./server/config/config')[env]; 
+var config = require('./server/config/config')[env];
 
 require('./server/config/express')(app, config);
 
